98be96dffda1ea3bf386d119d60ad95963a7e103
Gabriel Pineda <willesden10@gmail.com>
2021-08-15 15:47:19 +0100

    First commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java World!");
+
+        //Is code really compilling this project?
+        System.out.println("It's compilling, yes.");
+
+        //Is it still working?
+        System.out.println("Yes, it is still working");
+    }
+}
