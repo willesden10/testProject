6270fd880c29fef92eb31993ac74ff52a8b3348a
Gabriel Pineda <willesden10@gmail.com>
2021-08-15 16:00:09 +0100

    Commiting before deleting local rep

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java World!");
         System.out.println("Let's get serious.");
+        System.out.println("And let's mess up :-)");
     }
 }
